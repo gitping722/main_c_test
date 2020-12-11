cfd98c9b6aed62038e14a3968a41c8ec45d711af
gitping722 <515836722@qq.com>
2020-12-11 21:43:54 +0800

    update main.c

--- main.c
+++ main.c
@@ -5,10 +5,7 @@
 
 int main(void)
 {
-<<<<<<< HEAD
-
-=======
+    printf("merge into master");
     printf("remote branch2 test");
->>>>>>> 622d6d12c2957a7da586976a6e3f0d16dfb6d1f4
     return 0;
 }
